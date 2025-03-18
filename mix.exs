d5d67c65bc000e52f92b0687cd60c05dd04f9dc4
Felix Klein <felix@felixklein.com>
2025-03-18 10:00:27 +0100

    update libraries

--- mix.exs
+++ mix.exs
@@ -22,12 +22,12 @@ defmodule ExCldrUnitsRepro.MixProject do
   defp deps do
     [
       {:gettext, "~> 0.26.0"},
-      {:ex_cldr, "~> 2.40.0"},
+      {:ex_cldr, "~> 2.41.0"},
       {:ex_cldr_languages, "~> 0.3.0"},
-      {:ex_cldr_numbers, "~> 2.33.0"},
-      {:ex_cldr_units, "~> 3.17.0"},
+      {:ex_cldr_numbers, "~> 2.34.0"},
+      {:ex_cldr_units, "~> 3.18.0"},
       # fmt
-      {:styler, "~> 1.1", only: [:dev, :test], runtime: false}
+      {:styler, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
